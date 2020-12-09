a10243aa280c38b44edccaaf28db5bbb9a7de5bd
lorraineros <43746757+lorraineros@users.noreply.github.com>
2020-12-09 21:45:40 +0000

    Smá done með rúmmál snúða

--- lokaverk.py
+++ lokaverk.py
@@ -19,18 +19,18 @@ def heilda(fall):
             i = ""
             ind = x.index("x")
             if ind == 0 and len(x) > 1:
-                i = i + str(1 / (int(x[ind + 1:]) + 1)) + "*(x)**" + str(int(x[ind + 1:]) + 1)
+                i = i + str(1 / (int(x[ind + 1:]) + 1)) + "*x**" + str(int(x[ind + 1:]) + 1)
                 lidur.append(i)
             elif ind == 0 and len(x) == 1:
-                lidur.append("0.5*(x)**2")
+                lidur.append("0.5*x**2")
             elif len(x) > 2:
-                i = i + str(int(x[:ind]) / (int(x[ind + 1:]) + 1)) + "*(x)**" + str(int(x[ind + 1:]) + 1)
+                i = i + str(int(x[:ind]) / (int(x[ind + 1:]) + 1)) + "*x**" + str(int(x[ind + 1:]) + 1)
                 lidur.append(i)
             else:
-                i = i + str(int(x[:ind]) / 2) + "*(x)**" + "2"
+                i = i + str(int(x[:ind]) / 2) + "*x**" + "2"
                 lidur.append(i)
         else:
-            lidur.append(x + "*(x)")
+            lidur.append(x + "*x")
     for x in range(len(lidur)):
         strengur = strengur + formerki[x] + lidur[x]
 
@@ -44,12 +44,16 @@ def flatarmal(fall, efri, nedri):
 
     return flatarmal
 
-def rummal():
-    pass
+def rummalSnuda(fall):
+    nytt_fall = heilda(fall)
+    m_a = nytt_fall.replace("x", efri)
+    m_b = nytt_fall.replace("x", nedri)
+    rummal = float(math.pi * ((heilda(fall))**2))
+    return rummal
 
 fall = input("Sláðu inn fallið: f(x)= ")
 efri = input("Sláðu inn x fyrir efri mörk: ")
 nedri = input("Sláðu inn x fyrir neðri mörk: ")
 
 print("Flatarmálið milli x-ás og f(x) =", fall, "er:", flatarmal(fall,efri,nedri))
-# print("Rúmmmálið milli x-ás og f(x) =", fall, "er:", rummal())
+print("Rúmmmál Snúða f(x) =", fall, "er:", rummalSnuda(fall))
